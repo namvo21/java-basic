81431b9af8c54e53ad6b416c616498653c29b064
namvo21 <fossilq2110@gmail.com>
2021-11-05 23:18:13 +0700

    Bai Tap Lesson 2 & 3

--- src/lesson3/CovertStringToTime.java
+++ src/lesson3/CovertStringToTime.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class CovertStringToTime {
     public static void main(String[] args) {
+        String myString = "3hrs and 50 minutes";
+        String [] splitStr = myString.split(" and ");
 
+        String hourStr = splitStr[0];
+        String minStr = splitStr[1];
+
+        long hours = Long.parseLong(hourStr.replaceAll("[^0-9]", ""));
+        long minutes = Long.parseLong(minStr.replaceAll("[^0-9]", ""));
+
+        long totalMinutes = (hours * 60) + minutes;
+        System.out.printf("Total minutes: %d %s", totalMinutes , "minutes");
     }
 }
